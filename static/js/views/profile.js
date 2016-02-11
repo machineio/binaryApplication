a5fe3fc2f05f1ae30ff3d4257824e8817b3012d2
Jean Chassoul <chassoul@gmail.com>
2016-02-11 11:49:12 -0800

    go

--- static/js/views/profile.js
+++ static/js/views/profile.js
@@ -104,6 +104,66 @@ fun.views.profile = Backbone.View.extend({
         }, 2);
     },
 
+
+    renderTickGraph: function(){
+        var ws = new WebSocket("ws://" + location.host + "/ws/alerts");
+
+
+
+        var $placeholder = $('#fun-binary-second-graph');
+        var datalen = 100;
+        var plot = null;
+        var series = {
+            label: "Value",
+            lines: { 
+                show: true,
+                fill: true,
+                shadowSize: 25
+            },
+            points: {
+                show:true
+            },
+            data: []
+        };
+        ws.onmessage = function(evt) {
+
+
+            var d = $.parseJSON(evt.data);
+            console.log(d);
+            series.data.push([d.x, d.y]);
+            while (series.data.length > datalen) {
+                series.data.shift();
+            }
+            if(plot) {
+                plot.setData([series]);
+                plot.setupGrid();
+                plot.draw();
+            } else if(series.data.length > 10) {
+                plot = $.plot($placeholder, [series], {
+                    xaxis:{
+                        mode: "time",
+                        timeformat: "%H:%M:%S",
+                        minTickSize: [2, "second"],
+                    },
+                    yaxis: {
+                        min: 0,
+                        max: 5
+                    }
+                });
+                plot.draw();
+            }
+        }
+        ws.onopen = function(evt) {
+            $('#conn_status').html('<b>Connected</b>');
+        }
+        ws.onerror = function(evt) {
+            $('#conn_status').html('<b>Error</b>');
+        }
+        ws.onclose = function(evt) {
+            $('#conn_status').html('<b>Closed</b>');
+        }
+    },
+
     renderBinaryGraph: function(){
         /*
          * Flot Interactive Chart
