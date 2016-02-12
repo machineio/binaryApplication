029431307409849201022f76f7d4f61b022875da
Jean Chassoul <chassoul@gmail.com>
2016-02-12 02:58:46 -0800

    go

--- static/js/views/profile.js
+++ static/js/views/profile.js
@@ -41,7 +41,7 @@ fun.views.profile = Backbone.View.extend({
         this.renderTickGraph();
 
         $('#clock').countdown('2020/10/10', function(event) {
-            $(this).html(event.strftime('%D days %H:%M:%S'));
+            $(this).html(event.strftime('%H:%M:%S'));
         });
 
         // startDate = new Date(2013, 6, 25);
