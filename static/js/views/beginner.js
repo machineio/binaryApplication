525fee93bd19ee54038a4a985ee0e58ec38ee946
Jean Chassoul <chassoul@gmail.com>
2016-02-19 09:03:50 -0800

    q

--- static/js/views/beginner.js
+++ static/js/views/beginner.js
@@ -1,4 +1,4 @@
-fun.views.begginer = Backbone.View.extend({
+fun.views.beginner = Backbone.View.extend({
 
     /*
     * Bind the event functions to the different HTML elements
@@ -11,15 +11,15 @@ fun.views.begginer = Backbone.View.extend({
     * Class constructor
     */
     initialize : function(options) {
-        fun.containers.begginer = this.$el;
+        fun.containers.beginner = this.$el;
     },
     
     /*
-    * Render the begginer view
+    * Render the beginner view
     */
     render : function(){
         if (!this.$el.html()){
-            var template = _.template(fun.utils.getTemplate(fun.conf.templates.begginer));
+            var template = _.template(fun.utils.getTemplate(fun.conf.templates.beginner));
             this.$el.html(template);
         }
         this.$el.removeClass("hide").addClass("show");
